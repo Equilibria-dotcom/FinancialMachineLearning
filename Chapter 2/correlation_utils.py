2606922d57f240ae00f0b1034a1142b9647ee1cb
matteo <equilibria1610@gmail.com>
2024-07-30 21:32:26 +0100

    fitting Pachenko distribution for random correlation

--- Chapter 2/correlation_utils.py
+++ Chapter 2/correlation_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from sklearn.neighbors import KernelDensity
 
 def mpPDF(var, q, pts):
     # Marcenko-pastur pdf
@@ -9,3 +10,27 @@ def mpPDF(var, q, pts):
     pdf = q / (2 * np.pi * var * eVal) * ((eMax - eVal) * (eVal - eMin)) ** .5
     pdf = pd.Series(pdf, index=eVal)
     return pdf
+
+
+def getPCA(matrix):
+    # get eVal, eVec from a Hermitian Matrix
+    eVal, eVec = np.linalg.eigh(matrix)
+    indices = eVal.argsort()[::-1]
+    eVal, eVec = eVal[indices], eVec[:, indices]
+    eVal = np.diagflat(eVal)
+    return eVal, eVec
+
+def fitKDE(obs, bWidth=.25, kernel='gaussian',x=None):
+    # fit kernel to a series of obs and derive the prob of obs
+    if len(obs.shape)==1:obs.reshape(-1,1)
+    kde=KernelDensity(kernel=kernel,bandwidth=bWidth).fit(obs)
+    if x is None:x=np.unique(obs).reshape(-1,1)
+    if len(x.shape)==1:x=x.reshape(-1,1)
+    logProb=kde.score_samples(x)
+    pdf=pd.Series(np.exp(logProb),index=x.flatten())
+    return pdf
+
+x=np.random.normal(size=(10000,1000))
+eVal0,eVec0=getPCA(np.corrcoef(x,rowvar=0))
+pdf0=mpPDF(1.,q=x.shape[0]/float(x.shape[1]),pts=1000)
+pdf1=fitKDE(np.diag(eVal0).reshape(-1, 1),bWidth=.01)
